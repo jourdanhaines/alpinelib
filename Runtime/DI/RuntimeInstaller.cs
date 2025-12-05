dacb1081efe9ad3b233a7ac60a0fc10494b321cb
Jourdan <58449338+jourdanhaines@users.noreply.github.com>
2025-12-05 22:41:25 +1100

    feat: inject at top-level via runtimeinstaller

---
+++ Runtime/DI/RuntimeInstaller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace AlpineLib.DI {
+    [DefaultExecutionOrder(-10000)]
+    internal sealed class RuntimeInstaller : MonoBehaviour {
+        private static bool _created;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Install() {
+            if (_created) return;
+            _created = true;
+
+            var go = new GameObject("AlpineLib.Injector");
+            DontDestroyOnLoad(go);
+
+            go.AddComponent<Injector>();
+        }
+    }
+}
